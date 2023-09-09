2876b03f5ffb26da8206d276eed8f4dd9c4f31ff
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-09 14:08:03 +0000

    Investigate Math methods

--- basics/06_num_and_math.js
+++ basics/06_num_and_math.js
@@ -1,18 +1,25 @@
-// Number
+           // Number
 
 const num = 400
 
-console.log(num);
+// console.log(num);
 
 const balance = new Number(500)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toFixed(2));
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
 
 const otherNumber = 125.789
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 const bigNumber = 100000;
-console.log(bigNumber.toLocaleString())
-console.log(bigNumber.toLocaleString('en-PK'));
+// console.log(bigNumber.toLocaleString())
+// console.log(bigNumber.toLocaleString('en-PK'));
+
+
+          //* ************************************************* *//
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.9));
