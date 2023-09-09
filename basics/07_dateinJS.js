2870a482bca58a7accdeeb3a47a730789e3c56ad
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-09 15:12:12 +0000

    js date method

--- basics/07_dateinJS.js
+++ basics/07_dateinJS.js
@@ -1,6 +1,8 @@
 //  Dates ///
 
 let myDate = new Date()
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
