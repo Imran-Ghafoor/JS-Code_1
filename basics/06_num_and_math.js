5b765aa2445ed826997ac2ee45a4aa897bf3b8ee
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-09 14:36:01 +0000

    Js Math method

--- basics/06_num_and_math.js
+++ basics/06_num_and_math.js
@@ -30,3 +30,12 @@ const bigNumber = 100000;
 // console.log(Math.max(2,4,6,8,9,10));
 
 console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.random()*10 + 1);
+
+const min = 10
+const max = 20
+
+// console.log(Math.random() * (min - max + 1) + min);    // we need value without digit
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
