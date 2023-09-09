3f62fe6b66cb1833fed05ebbe4d145294b63ef98
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-09 15:13:54 +0000

    js daate methods

--- basics/07_dateinJS.js
+++ basics/07_dateinJS.js
@@ -4,5 +4,8 @@ let myDate = new Date()
 // console.log(myDate);
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
