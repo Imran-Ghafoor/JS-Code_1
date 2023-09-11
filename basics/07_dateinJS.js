de67394391403d65250c82a2eb36c95828971112
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-11 17:40:51 +0000

    dateIn js

--- basics/07_dateinJS.js
+++ basics/07_dateinJS.js
@@ -20,5 +20,13 @@ let myCreatedDate = new Date("2023-01-15")         // date in dd/mm/yy
 // console.log(myCreatedDate.toDateString());
 
 let myTime = Date.now()
-console.log(myTime);
-console.log(myCreatedDate.getTime());
+// console.log(myTime);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now()/1000);    //when value come with decimal 
+// console.log(Math.floor(Date.now()/1000));    //apply in this form
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
