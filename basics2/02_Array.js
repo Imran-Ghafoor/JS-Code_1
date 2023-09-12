ec02484beece3bcd016eaa3d192f3d8c39020d95
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-12 13:33:20 +0000

    Investigate Array methods

--- basics2/02_Array.js
+++ basics2/02_Array.js
@@ -11,4 +11,20 @@
 
 const all_New_Heros = [...heroName, ...ourHero]
 
-console.log(all_New_Heros);
+// console.log(all_New_Heros);
+
+const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = anotherArray.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("imran"));
+console.log(Array.from("imran"));
+console.log(Array.from({name: "imran"}));  // value return empty array /// intrusting
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
