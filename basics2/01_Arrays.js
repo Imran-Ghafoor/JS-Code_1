9cacc258eeaaab9789a99e696516b04472dddddd
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-12 12:27:16 +0000

    Methods of Array

--- basics2/01_Arrays.js
+++ basics2/01_Arrays.js
@@ -16,4 +16,33 @@
  myArr.pop()        // remove the value from the end of array
  myArr.pop()
 
- console.log(myArr);
+ myArr.unshift(0)    // Add the value from the start of array
+ myArr.unshift(9)
+
+ myArr.shift()       // remove the value from the strat of the array
+ myArr.shift()
+
+ console.log(myArr.includes(9));    // in this method we ask the question
+ console.log(myArr.includes(4));
+
+console.log(myArr.indexOf(4));         // to find the index of the number
+
+const newArr = myArr.join()
+console.log(myArr);
+ console.log(newArr);
+
+// ++++ slice, Splice ++++ //
+
+console.log("A", myArr);
+
+const myArr3 = myArr.slice(1,3)
+
+console.log(myArr3);
+console.log("B ", myArr);
+ 
+
+const myArr4 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myArr4);
+
+
