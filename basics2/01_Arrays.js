d22df212ba674a9f883b0785170cabfdeb42cf3e
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-12 11:46:44 +0000

    Array push method

--- basics2/01_Arrays.js
+++ basics2/01_Arrays.js
@@ -4,8 +4,13 @@
 
  const myHerro = ["imrankhan", "Iqbal"]
 
-
-
  const myArr2 = new Array(0,1,2,3,4)
 
- console.log(myArr[2]);
+//  console.log(myArr[2]);
+
+ //++++++++ Array Methods ++++++//
+
+ myArr.push(6)
+ myArr.push(7)
+
+ console.log(myArr);
