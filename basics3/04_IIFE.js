cc8cf3c9935e9709cf13a36928d3799668af81d6
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-18 14:44:50 +0000

    iifi function

---
+++ basics3/04_IIFE.js
@@ -0,0 +1,13 @@
+// ++++ Immediately Invoked Function Expressions +++++// 
+
+(function iifi (){
+
+//named iifi
+    console.log(`DB Connected`);
+
+}) ();        // semi colon ; is important in Invoke function when write two iifi function ... for better where the function is break
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+
+})("imran")
