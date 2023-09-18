8a3e41e371d890ae35b6341f805d8c9b3090b244
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-18 11:21:29 +0000

    Object in function

--- basics3/01_function.js
+++ basics3/01_function.js
@@ -29,7 +29,35 @@ function loginUserMessage(username){
 }
 // console.log(loginUserMessage("Imran"))
 
-function calculateCartPrice(num1){
+function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
-console.log (calculateCartPrice(2));
+// console.log (calculateCartPrice(2));
+// console.log(calculateCartPrice(200,400,600,900));
+
+const user= {                   // Object create
+    username: "imran",
+    price: 299
+    // prices: 299             // type checking important
+}
+
+function handleObject(anyobject){            // Now in this how to use object in function
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price} `);
+
+}
+// handleObject(user)
+
+// other way to define the object
+handleObject({
+    username: "khan",
+    price: 300
+})
+
+const myNewArray = [200, 400, 100, 900]           // pass the Array in function
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200 ,400, 100, 900]));
+
