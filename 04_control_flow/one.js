4c3bddce6b8bc6959af556024e7e78253470ceca
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-23 19:02:43 +0500

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -5,11 +5,22 @@
 const isUserLoggedIn = true
 const temperature = 60
 
-if(temperature === 70){
-console.log("less than 60");
-}else{
-    console.log("temp grater than 60");
-}
-console.log("executed");
+// if(temperature === 70){
+// console.log("less than 60");
+// }else{
+//     console.log("temp grater than 60");
+// }
+// console.log("executed");
 
 // <,>, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+// if(score > 100){
+//     let power = "fly"f
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);        // scope concept
+
+
+// switch statement
+
