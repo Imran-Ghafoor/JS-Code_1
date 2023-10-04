91f84c6b296cc9fc0b28bcfb2c2c7ae80780b1f6
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-04 17:46:33 +0500

    Investigate Operators

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,8 +2,8 @@
 // If statment
 
 
-const isUserLoggedIn = true
-const temperature = 60
+// const isUserLoggedIn = true
+// const temperature = 60
 
 // if(temperature === 70){
 // console.log("less than 60");
@@ -21,4 +21,18 @@ const temperature = 60
 // }
 // console.log(`User Power: ${power}`);        // scope concept
 
-// switch statment
+// reall life example 
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromGmail = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGmail || loggedInFromGmail){
+    console.log("User Logged In");
+}
+
