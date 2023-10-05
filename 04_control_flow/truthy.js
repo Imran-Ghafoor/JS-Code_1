0091e10295f98aeb059875e782f10459ed9a4c78
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-05 20:04:52 +0500

    truthy falsy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,15 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user Email");
+} else {
+    console.log("Email not found");
+}
+
+// Falsy values
+
+// false , 0 ,-0 , BigInt 0n , "" , null , undefined , NaN ,
+
+// Truthy Value
+
+// "0" , [] , 'false' , " " , {} , funciton(){} ,  
