a8c51e3a7a54e832b226964a103bd3186555f3cf
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-05 19:05:07 +0500

    switch-cse

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,11 +1,26 @@
 // switch statment 
 //investigate switch oprator
 
-switch (key) {
-    case value:
-        
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+
+        break;
+    case 2:
+        console.log("Febraury");
+
+        break;
+    case 3:
+        console.log("March");
+
+        break;
+    case 4:
+        console.log("April");
+
         break;
 
     default:
+        console.log("default case match");
         break;
 }
