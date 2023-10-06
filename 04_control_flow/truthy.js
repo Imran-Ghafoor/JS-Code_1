2a0cc4730b385e7e5bf5ce5ba2563195a71f378f
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-06 20:07:23 +0500

    truthy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -13,3 +13,13 @@ if (userEmail) {
 // Truthy Value
 
 // "0" , [] , 'false' , " " , {} , funciton(){} ,  
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}   
