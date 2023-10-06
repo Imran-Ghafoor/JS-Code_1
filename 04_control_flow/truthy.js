f2c27b133955bd847f187b0f265ced1b868fb768
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-06 20:51:09 +0500

    Terniary Operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -34,3 +34,10 @@ val1 = null ?? 10 ?? 20
 
 
 console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false             
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("Less than 80") : console.log("greater than 80");
