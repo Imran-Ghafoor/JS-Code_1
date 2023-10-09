065e520fbf02c83055a6331f713d0b1bba16bef3
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-10 00:19:57 +0500

    Break in loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -29,9 +29,19 @@ console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
 
-    console.log(element);
+    // console.log(element);
 
 }
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`);
+        break
+    }
+    console.log(`value of i is ${i}`);
+
+}
+
 
 
 
