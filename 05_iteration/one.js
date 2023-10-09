021a56a6c53087b181578ae3f9584b02bcf5294f
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-10 00:25:36 +0500

    braek statment

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -35,14 +35,14 @@ for (let index = 0; index < myArray.length; index++) {
 
 // break statment
 
-// for (let i = 1; i <= 20; i++) {
-//     if (i == 5) {
-//         console.log(`Detected 5`);
-//         break
-//     }
-//     console.log(`value of i is ${i}`);
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`);
+        break
+    }
+    console.log(`value of i is ${i}`);
 
-// }
+}
 
 // continue statment
 
