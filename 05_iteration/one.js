52d1b2f4f769fd4f06a973cb07fb850c5e9aee03
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-10 00:11:05 +0500

    Loop with array

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,23 +1,35 @@
 // For Loop
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    // if (element == 5) {
-    //     console.log("5 is best number");
-    // } 
-    // console.log(element);
-}
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+// if (element == 5) {
+//     console.log("5 is best number");
+// } 
+// console.log(element);
+// }
 
-for (let i = 1; i <= 5; i++) {
-    // console.log(`outer loop value:${i}`);
-    for (let j = 1; j <= 5; j++) {
-        // console.log(`inner loop value:${j} and outer loop ${i}`);
+// for (let i = 1; i <= 5; i++) {
+// console.log(`outer loop value:${i}`);
+// for (let j = 1; j <= 5; j++) {
+// console.log(`inner loop value:${j} and outer loop ${i}`);
 
-        console.log(i + '*' + j + ' = ' + i * j);
+// console.log(i + '*' + j + ' = ' + i * j);
 
 
 
-    }
+// }
+
+// }
+
+// loop with array
+
+let myArray = ['superman', 'batman', 'spiderman']
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+
+    console.log(element);
 
 }
 
