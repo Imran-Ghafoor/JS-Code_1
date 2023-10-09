6ac41235bfce27ea4ffc8c64b25936f54ceb346e
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-09 23:58:09 +0500

    Loop In Loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -2,9 +2,23 @@
 
 for (let i = 0; i <= 10; i++) {
     const element = i;
-    if (element == 5) {
-        console.log("5 is best number");
+    // if (element == 5) {
+    //     console.log("5 is best number");
+    // } 
+    // console.log(element);
+}
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`outer loop value:${i}`);
+    for (let j = 0; j <= 5; j++) {
+        console.log(`inner loop value:${j} and outer loop ${i}`);
+
+
+
     }
-    console.log(element);
 
 }
+
+
+
+
