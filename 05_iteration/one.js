d74efe816fdf8dc6cfed5a1bcdad55907ae2be28
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-09 23:32:28 +0500

    for Loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,6 +1,7 @@
 // For Loop
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    console.log(element);
 
 }
