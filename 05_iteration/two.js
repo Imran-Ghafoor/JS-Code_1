e8ccabbecadf4d08df5b916d26b433fb9c2ad064
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-10 16:47:52 +0500

    Invstgate while loop

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -1,9 +1,17 @@
 
-let index = 0
-while (index <= 10) {
-    console.log(`value of index is ${index}`);
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
 
-    index = index + 2
+//     index = index + 2
+// }
 
+let myArray = ['flash', 'superman', 'batman']
+console.log(myArray.length);
+let arr = 0
 
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+
+    arr = arr + 1
 }
