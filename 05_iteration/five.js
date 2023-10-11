091b1763aaaa40f82f2865ffeff4ff49450614a1
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-11 18:41:25 +0500

    parameter values

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -6,13 +6,22 @@ const coding = ['js', 'py', 'java', 'ruby', 'cpp']
 //     console.log(items);
 // })
 
-// coding.forEach((items) => {       // Arrow function also use in foreach loop
+// coding.forEach((items) => {    // Arrow function also use in foreach loop
 // console.log(items);
 // })
 
 
 // Another way
-function printMe(items) {
-    console.log(items);
-}
-coding.forEach(printMe)
+// function printMe(items) {
+//     console.log(items);
+// }
+// coding.forEach(printMe)
+
+
+coding.forEach((items, index, arr) => {   // loop have many parameter value
+    console.log(items, index, arr);
+})
+
+
+
+
