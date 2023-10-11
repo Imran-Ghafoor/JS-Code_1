a8be52ca61a80339c391af8303c748de4058257b
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-11 18:32:00 +0500

    foreach loop

---
+++ 05_iteration/five.js
@@ -0,0 +1,12 @@
+// foreach Loop
+
+const coding = ['js', 'py', 'java', 'ruby', 'cpp']
+
+// coding.forEach(function (items) {
+//     console.log(items);
+// })
+
+coding.forEach((items) => {       // Arrow function also use in foreach loop
+    console.log(items);
+})
+
