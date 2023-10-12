58063bb2853e901f6a4c0963dc27073458774b64
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-12 20:23:34 +0500

    shopingCart Reduce

--- 05_iteration/eight.js
+++ 05_iteration/eight.js
@@ -2,8 +2,34 @@
 
 const myNum = [1, 2, 3,]
 const myTottal = myNum.reduce(function (accumulator, currentvalue) {
-    console.log(`acc: ${accumulator} and currval:${currentvalue} `);
+    // console.log(`acc: ${accumulator} and currval:${currentvalue} `);
     return accumulator + currentvalue
 }, 0)
 
-console.log(`Our tottal value: ${myTottal}`);
+// console.log(`Our tottal value: ${myTottal}`);
+
+const shopingCart = [
+    {
+        itemName: 'Bread',
+        price: 250
+    },
+    {
+        itemName: 'Egg',
+        price: 300
+    },
+    {
+        itemName: 'Milk',
+        price: 170
+    },
+    {
+        itemName: 'Rice',
+        price: 400
+    },
+    {
+        itemName: 'Tea',
+        price: 100
+    },
+]
+
+const priceToPay = shopingCart.reduce((accoumudate, item) => accoumudate + item.price, 0)
+console.log(priceToPay);
