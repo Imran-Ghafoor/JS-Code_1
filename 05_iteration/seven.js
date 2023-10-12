483a8a9e6d35c6f4db300f64ee451d9706d395da
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-12 19:14:20 +0500

    Investigate map

---
+++ 05_iteration/seven.js
@@ -0,0 +1,9 @@
+// Map
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNumbers = myNumbers.map((num) => num + 10)
+// console.log(newNumbers);
+
+//     with foreach Loop
+myNumbers.forEach((num) => {
+    console.log(num + 10);
+})
