779a1dcf0d0b7001252cd9a406d6a137fb8d17a4
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-12 19:25:51 +0500

    Map

--- 05_iteration/seven.js
+++ 05_iteration/seven.js
@@ -5,5 +5,13 @@ const newNumbers = myNumbers.map((num) => num + 10)
 
 //     with foreach Loop
 myNumbers.forEach((num) => {
-    console.log(num + 10);
+    // console.log(num + 10);
 })
+
+// Channing method
+const newNum = myNumbers                // in which we use methods multitimes
+    .map((num) => num * 10)
+    .map((num) => num + 1)
+    .filter((num) => num <= 50)
+
+console.log(newNum);
